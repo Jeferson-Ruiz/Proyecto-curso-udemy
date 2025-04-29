5a0a901dd8b042fb3c9ec8b173d8fb4fedf1073a
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-04-29 10:56:03 -0500

    Creacion de intercace ProductFeignClient con FeignClient

---
+++ msvc-items/src/main/java/com/jeferson/springcloud/msvc/items/clients/ProductFeignClient.java
@@ -0,0 +1,17 @@
+package com.jeferson.springcloud.msvc.items.clients;
+
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.jeferson.springcloud.msvc.items.models.ProductDto;
+
+@FeignClient(url = "localhost:8080")
+public interface ProductFeignClient {
+
+    @GetMapping
+    List<ProductDto> listProducts();  
+
+    @GetMapping
+    ProductDto details(@PathVariable Long id);
+}
