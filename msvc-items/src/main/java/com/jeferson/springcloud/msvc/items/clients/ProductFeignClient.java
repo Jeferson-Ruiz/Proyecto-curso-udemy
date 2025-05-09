315383b62b5ba54166a2f9fa065b519a6ee4dc58
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-09 11:33:17 -0500

    Configuracion y depencia de EurekaClient

--- msvc-items/src/main/java/com/jeferson/springcloud/msvc/items/clients/ProductFeignClient.java
+++ msvc-items/src/main/java/com/jeferson/springcloud/msvc/items/clients/ProductFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.jeferson.springcloud.msvc.items.models.ProductDto;
 
-@FeignClient(url = "localhost:8080", name = "msvc-products")
+@FeignClient(name = "msvc-products")
 public interface ProductFeignClient {
 
     @GetMapping
