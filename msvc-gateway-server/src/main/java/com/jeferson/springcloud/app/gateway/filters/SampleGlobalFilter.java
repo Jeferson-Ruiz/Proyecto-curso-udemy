acbd796e2e70de5d5a87f9ad198a5d674481c923
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-14 08:53:58 -0500

    Modificar el response en el filtro Post

--- msvc-gateway-server/src/main/java/com/jeferson/springcloud/app/gateway/filters/SampleGlobalFilter.java
+++ msvc-gateway-server/src/main/java/com/jeferson/springcloud/app/gateway/filters/SampleGlobalFilter.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -19,6 +21,9 @@ public class SampleGlobalFilter implements GlobalFilter {
 
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
             logger.info("Ejecutando el filtro POST responde");
+
+            exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
+            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         }));
     }
 }
