35239419cf59960c75e4600e5ac5e5351be7654b
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-14 08:50:25 -0500

    Filtro globales Pre y Post

---
+++ msvc-gateway-server/src/main/java/com/jeferson/springcloud/app/gateway/filters/SampleGlobalFilter.java
@@ -0,0 +1,24 @@
+package com.jeferson.springcloud.app.gateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class SampleGlobalFilter implements GlobalFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Ejecutando el registro antes del Request PRE");
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            logger.info("Ejecutando el filtro POST responde");
+        }));
+    }
+}
