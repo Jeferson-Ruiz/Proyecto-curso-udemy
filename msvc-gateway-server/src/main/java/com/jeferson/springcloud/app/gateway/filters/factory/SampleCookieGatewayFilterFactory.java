1c860a71d6636623faecdc2fa6fee21755973611
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-05-19 10:44:33 -0500

    Configuracion application.yml FilterFactory segunda forma

--- msvc-gateway-server/src/main/java/com/jeferson/springcloud/app/gateway/filters/factory/SampleCookieGatewayFilterFactory.java
+++ msvc-gateway-server/src/main/java/com/jeferson/springcloud/app/gateway/filters/factory/SampleCookieGatewayFilterFactory.java
@@ -1,5 +1,7 @@
 package com.jeferson.springcloud.app.gateway.filters.factory;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,28 +12,40 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
-public class SampleCookieGatewayFilterFactory extends AbstractGatewayFilterFactory<SampleCookieGatewayFilterFactory.ConfigurationCookie> {
+public class SampleCookieGatewayFilterFactory
+        extends AbstractGatewayFilterFactory<SampleCookieGatewayFilterFactory.ConfigurationCookie> {
 
     private final Logger logger = LoggerFactory.getLogger(SampleCookieGatewayFilterFactory.class);
 
-    public SampleCookieGatewayFilterFactory(){
+    public SampleCookieGatewayFilterFactory() {
         super(ConfigurationCookie.class);
     }
 
     @Override
     public GatewayFilter apply(ConfigurationCookie config) {
-        return(exchange, chain) -> {
+        return (exchange, chain) -> {
 
-            logger.info("Ejecutando pre gateway de filter factory: "+ config.message);
-            
-            return chain.filter(exchange).then(Mono.fromRunnable(() ->{
-                Optional.ofNullable(config.value).ifPresent(cookie ->{
+            logger.info("Ejecutando pre gateway de filter factory: " + config.message);
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                Optional.ofNullable(config.value).ifPresent(cookie -> {
                     exchange.getResponse().addCookie(ResponseCookie.from(config.name, config.value).build());
                 });
-                
-                logger.info("Ejecutando pre gateway de filter factory: "+ config.message);
+
+                logger.info("Ejecutando pre gateway de filter factory: " + config.message);
             }));
         };
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList("message", "name", "value");
+    }
+
+    // Aplica cuando el sufijo SampleCookie es diferente en el application.yml
+    @Override
+    public String name() {
+        return "EjemploCookie";
     }
 
     public static class ConfigurationCookie {
@@ -40,26 +54,30 @@ public class SampleCookieGatewayFilterFactory extends AbstractGatewayFilterFacto
         private String value;
         private String message;
 
-
         public String getName() {
             return name;
         }
+
         public void setName(String name) {
             this.name = name;
         }
+
         public String getValue() {
             return value;
         }
+
         public void setValue(String value) {
             this.value = value;
         }
+
         public String getMessage() {
             return message;
         }
+
         public void setMessage(String message) {
             this.message = message;
         }
-        
+
     }
 
 }
