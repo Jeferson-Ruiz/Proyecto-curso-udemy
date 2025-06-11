f41a41ce655f8ecc071d929b1a0b95eaad1cad1b
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-06-11 12:30:32 -0500

    Anotacion @RefreshScope

--- msvc-items/src/main/java/com/jeferson/springcloud/msvc/items/controllers/ItemController.java
+++ msvc-items/src/main/java/com/jeferson/springcloud/msvc/items/controllers/ItemController.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,7 @@ import com.jeferson.springcloud.msvc.items.services.ItemService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
+@RefreshScope
 @RestController
 public class ItemController {
 
