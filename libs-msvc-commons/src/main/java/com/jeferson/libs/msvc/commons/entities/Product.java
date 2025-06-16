c06f3908ac94a7c6c2391b7baf95bfa210487cbd
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-06-16 10:40:42 -0500

    Agregar dependencias commons

---
+++ libs-msvc-commons/src/main/java/com/jeferson/libs/msvc/commons/entities/Product.java
@@ -0,0 +1,81 @@
+package com.jeferson.libs.msvc.commons.entities;
+
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+
+@Table(name = "productos")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "nombre", length = 20, nullable = false)
+    private String name;
+
+    @Column(name = "precio", nullable = false)
+    private Double price;
+
+    @Column(name = "creado_en", nullable = false)
+    private LocalDate createAt;
+
+    @Transient
+    private int port;
+
+    public Product(Long id, String name, Double price, LocalDate createAt, int port) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.createAt = createAt;
+        this.port = port;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setCreateAt(LocalDate createAt) {
+        this.createAt = createAt;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public Double getPrice() {
+        return price;
+    }
+
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+
+    public int getPort() {
+        return port;
+    }
+}
---
+++ libs-msvc-commons/src/test/java/com/jeferson/libs/msvc/commons/entities/LibsMsvcCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeferson.libs.msvc.commons.entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibsMsvcCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
