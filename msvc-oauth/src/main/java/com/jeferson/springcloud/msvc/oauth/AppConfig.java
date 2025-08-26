cec17ae54281f54a98ad2fb51eca5f12add5b7a8
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-08-26 13:03:23 -0500

    Propagando contecto de trazas con webclient y Oauth

--- msvc-oauth/src/main/java/com/jeferson/springcloud/msvc/oauth/AppConfig.java
+++ msvc-oauth/src/main/java/com/jeferson/springcloud/msvc/oauth/AppConfig.java
@@ -1,20 +1,30 @@
 package com.jeferson.springcloud.msvc.oauth;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.reactive.function.client.WebClient;
+// import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    @LoadBalanced
-    WebClient.Builder webCliente(){
-        return WebClient.builder().baseUrl("http://msvc-users");
+    WebClient webCliente(WebClient.Builder builder, 
+        ReactorLoadBalancerExchangeFilterFunction lbFunction){
+        return builder
+            .baseUrl("http://msvc-users")
+            .filter(lbFunction)
+            .build();
     }
+
+    // @Bean
+    // @LoadBalanced
+    // WebClient.Builder webCliente(){
+    //     return WebClient.builder().baseUrl("http://msvc-users");
+    // }
 
     @Bean
     PasswordEncoder passwordEncoder(){
