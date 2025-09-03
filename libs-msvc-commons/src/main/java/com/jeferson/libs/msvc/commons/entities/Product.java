7ff6c868103472c8204e381b7604ea815f242321
Jeferson Ruiz <jefersonruiz587@gmail.com>
2025-09-03 08:15:43 -0500

    Agregando anotacion @entity en commons

--- libs-msvc-commons/src/main/java/com/jeferson/libs/msvc/commons/entities/Product.java
+++ libs-msvc-commons/src/main/java/com/jeferson/libs/msvc/commons/entities/Product.java
@@ -2,13 +2,14 @@ package com.jeferson.libs.msvc.commons.entities;
 
 import java.time.LocalDate;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
-
+@Entity
 @Table(name = "productos")
 public class Product {
 
